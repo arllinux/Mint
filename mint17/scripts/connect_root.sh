e27af829b0fe01482985b0112ba57cb6c6708858
jpantinoux <jeanpierre.antinoux@free.fr>
2014-06-22 12:06:30 +0200

    Update connect_root.sh

--- mint17/scripts/connect_root.sh
+++ mint17/scripts/connect_root.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 #
-# emmenager.sh
+# connect_root.sh
 # 
-# (c) Niki Kovacs, 2014
-# Modifié par Jean-Pierre Antinoux - juin 2014
+# Jean-Pierre Antinoux - juin 2014
 
 
 # Création du mot de passe administrateur
